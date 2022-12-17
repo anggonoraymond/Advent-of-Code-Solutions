a3deb5d786a92d06f3162fbc795bd6b87d6bb817
anggonoraymond <59703635+anggonoraymond@users.noreply.github.com>
2022-12-17 12:15:17 -0800

    Day 1 Solution

---
+++ day1.py
@@ -0,0 +1,21 @@
+file = open("day1.txt","r")
+most_calorie = []
+current = 0
+while True:
+  input = file.readline()
+  if not input:
+    break;
+  if input == "\n":
+    if(len(most_calorie) < 3):
+      most_calorie.insert(0,current)
+    else:
+      if(current > min(most_calorie)):
+        most_calorie.remove(min(most_calorie))
+        most_calorie.insert(0,current)
+    current = 0
+  else:
+    current += int(input)
+file.close()
+
+print("Most calorie an elf hold:",max(most_calorie))
+print("Sum of the calorie the top 3 elves hold:",sum(most_calorie))
